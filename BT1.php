fc095c8bc94bd5b940d118d2539868e8001ad057
NguyenManh <manh95405@gmail.com>
2021-09-19 15:09:13 +0700

    upload 2 file Web

---
+++ BT1.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Restaunt Website</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <section class = "Food">
+        <div class = "container">
+            <div class = "logo">
+                <img src="img/logo.png" alt="" class = "img-responsive">
+            </div>
+            <div class = "menu text-right">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Foods</a></li>
+                    <li><a href="#">Contants</a></li>
+                </ul>
+            </div>
+            <div class = "Clearfix"></div>
+        </div>
+    </section>
+    <section class = "Food-search text-center">
+        <div class = "container">
+            <form action="">
+                <input type="search" name = "search" placeholder = "Search for food">
+                <input type="submit" name = "submit" value = "Search" class = "btm btn">
+            </form>
+            
+        </div>
+    </section>
+    <section class = "categorles">
+        <div class = "container">
+            <h2 class = "text-center">Categorles</h2>
+           <a href="">
+                <div class = "box-1 float-container">
+                <img src="img/pizza.jpg" alt="pizza" class = "img-responsive img-curre">
+                <h5 class = "float-text text-white">Pizza</h5>
+            </div>
+            <div class = "box-1 float-container">
+            <img src="img/burger.jpg" alt="Burger" class = "img-responsive img-curre">
+                <h5 class = "float-text text-white">Burger</h5>
+            </div>
+            <div class = "box-1 float-container">
+            <img src="img/momo.jpg" alt="Momo" class = "img-responsive img-curre">
+                <h5 class = "float-text text-white">Momo</h5>
+            </div>
+            </a>
+            <div class = "clearfix"></div>
+        </div>
+    </section>
+    <section class = "food-menu">
+        <div class = "container">
+            <h2 class = "text-center">Explore Foods</h2>
+            <div class = "food-menu-box">
+                    <div class = "food-menu-img">
+                    <img src="img/menu-pizza.jpg" alt="menu-pizza"class = "img-responsive img-curre">
+                    </div>
+                    <div class = "food-menu-desc">
+                        <h4>Food Title</h4>
+                        <p class = "food-price">$2.3</p>
+                        <p class = "food-detail">
+                            Made with Italian Souce, chicken, and organice vegetables.
+                        </p>
+                        <a href="#" class = "btn">Order Now</a>
+                        <div class = "clearfix"></div>
+                    </div>
+            </div>
+            <div class = "food-menu-box">
+                    <div class = "food-menu-img">
+                    <img src="img/menu-burger.jpg" alt="menu-burger"class = "img-responsive img-curre">
+                    </div>
+                    <div class = "food-menu-desc">
+                        <h4>Nice Burger</h4>
+                        <p class = "food-price">$2.3</p>
+                        <p class = "food-detail">
+                            Made with Italian Souce, chicken, and organice vegetables.
+                        </p>
+                        <a href="#" class = "btn">Order Now</a>
+                        <div class = "clearfix"></div>
+                    </div>
+            </div>
+            <div class = "food-menu-box">
+                    <div class = "food-menu-img">
+                    <img src="img/menu-burger.jpg" alt="menu-burger"class = "img-responsive img-curre">
+                    </div>
+                    <div class = "food-menu-desc">
+                        <h4>Nice Burger</h4>
+                        <p class = "food-price">$2.3</p>
+                        <p class = "food-detail">
+                            Made with Italian Souce, chicken, and organice vegetables.
+                        </p>
+                        <a href="#" class = "btn">Order Now</a>
+                        <div class = "clearfix"></div>
+                    </div>
+            </div>
+            <div class = "food-menu-box">
+                    <div class = "food-menu-img">
+                    <img src="img/menu-pizza.jpg" alt="menu-pizza"class = "img-responsive img-curre">
+                    </div>
+                    <div class = "food-menu-desc">
+                        <h4>Food Title</h4>
+                        <p class = "food-price">$2.3</p>
+                        <p class = "food-detail">
+                            Made with Italian Souce, chicken, and organice vegetables.
+                        </p>
+                        <a href="#" class = "btn">Order Now</a>
+                        <div class = "clearfix"></div>
+                    </div>
+            </div>
+            <div class = "food-menu-box">
+                    <div class = "food-menu-img">
+                    <img src="img/menu-pizza.jpg" alt="menu-pizza"class = "img-responsive img-curre">
+                    </div>
+                    <div class = "food-menu-desc">
+                        <h4>Food Title</h4>
+                        <p class = "food-price">$2.3</p>
+                        <p class = "food-detail">
+                            Made with Italian Souce, chicken, and organice vegetables.
+                        </p>
+                        <a href="#" class = "btn">Order Now</a>
+                        <div class = "clearfix"></div>
+                    </div>
+            </div>
+            <div class = "food-menu-box">
+                    <div class = "food-menu-img">
+                    <img src="img/menu-momo.jpg" alt="menu-momo"class = "img-responsive img-curre">
+                    </div>
+                    <div class = "food-menu-desc">
+                        <h4>Checken Steam Momo</h4>
+                        <p class = "food-price">$2.3</p>
+                        <p class = "food-detail">
+                            Made with Italian Souce, chicken, and organice vegetables.
+                        </p>
+                        <a href="#" class = "btn">Order Now</a>
+                        <div class = "clearfix"></div>
+                    </div>
+            </div>
+            <div class = "clearfix"></div>
+        </div>
+   </section>
+    <section class = "social">
+       <div class = "container text-center">
+           <ul>
+               <li><a href="#"><img src="https://img.icons8.com/color/48/000000/facebook-new.png"/></a></li>
+               <li><a href="#"><img src="https://img.icons8.com/color/48/000000/instagram-new--v1.png"/></a></li>
+               <li><a href="#"><img src="https://img.icons8.com/color/48/000000/twitter--v1.png"/></a></li>
+           </ul>
+       </div>
+   </section>
+   <section class = "Foodter">
+       <div class = "container text-center">
+           <p>All rights reserved,Designed by <a href="#">pay manh</a></p>
+       </div>
+   </section>
+</body>
+</html>
